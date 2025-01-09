acsone/sale-workflow

[IMP] do not copy the workflow_process_id when copying the models
problem : if you copy it, the move/SO can be processed by the job too fast for the user to be able to perform his job

sale_automatic_workflow/tests/test_automatic_workflow.py
@@ -149,6 +149,22 @@ class TestAutomaticWorkflow(TestCommon, TestAutomaticWorkflowMixin):
         invoice = sale.invoice_ids
         self.assertEqual(invoice.journal_id.id, new_sale_journal.id)
 
+    def test_no_copy(self):
+        workflow = self.create_full_automatic()
+        sale = self.create_sale_order(workflow)
+        self.run_job()
+        invoice = sale.invoice_ids
+        self.assertTrue(sale.workflow_process_id)
+        self.assertTrue(invoice.workflow_process_id)
+        sale2 = sale.copy()
+        invoice2 = invoice.copy()
+        self.assertFalse(sale2.workflow_process_id)
+        self.assertFalse(invoice2.workflow_process_id)
+        picking = sale.picking_ids
+        self.assertTrue(picking.workflow_process_id)
+        picking2 = picking.copy()
+        self.assertTrue(picking2.workflow_process_id)
+
     def test_automatic_sale_order_confirmation_mail(self):
         workflow = self.create_full_automatic()
         workflow.send_order_confirmation_mail = True

adhoc-cicd/oca-sale-workflow

sale_automatic_workflow/tests/test_automatic_workflow.py
@@ -134,6 +134,22 @@ class TestAutomaticWorkflow(TestCommon, TestAutomaticWorkflowMixin):
         invoice = sale.invoice_ids
         self.assertEqual(invoice.journal_id.id, new_sale_journal.id)
 
+    def test_no_copy(self):
+        workflow = self.create_full_automatic()
+        sale = self.create_sale_order(workflow)
+        self.run_job()
+        invoice = sale.invoice_ids
+        self.assertTrue(sale.workflow_process_id)
+        self.assertTrue(invoice.workflow_process_id)
+        sale2 = sale.copy()
+        invoice2 = invoice.copy()
+        self.assertFalse(sale2.workflow_process_id)
+        self.assertFalse(invoice2.workflow_process_id)
+        picking = sale.picking_ids
+        self.assertTrue(picking.workflow_process_id)
+        picking2 = picking.copy()
+        self.assertFalse(picking2.workflow_process_id)
+
     def test_automatic_sale_order_confirmation_mail(self):
         workflow = self.create_full_automatic()
         workflow.send_order_confirmation_mail = True