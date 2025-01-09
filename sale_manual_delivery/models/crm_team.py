isabella232/sale-workflow

[MIG] sale_manual_delivery: Migration to 13.0

sale_manual_delivery/models/crm_team.py
@@ -1,5 +1,6 @@
 # Copyright 2017 Denis Leemann, Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from odoo import fields, models
 
 
@@ -8,8 +9,8 @@ class CrmTeam(models.Model):
 
     manual_delivery = fields.Boolean(
         string="Manual Delivery",
+        help="If Manual, the deliveries are not created at SO confirmation. "
+        "You need to use the Create Delivery button in order to reserve and "
+        "ship the goods.",
         default=False,
-        help="If Manual, the deliveries are not created at SO confirmation.\
-        You need to use the Create Delivery button in order to reserve and \
-        ship the goods.",
     )

sale_manual_delivery/models/sale_order.py
@@ -1,8 +1,9 @@
 # Copyright 2017 Denis Leemann, Camptocamp SA
+# Copyright 2021 Iván Todorovich, Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import api, fields, models
+
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
-from odoo.tools.translate import _
 
 
 class SaleOrder(models.Model):
@@ -11,38 +12,28 @@ class SaleOrder(models.Model):
     manual_delivery = fields.Boolean(
         string="Manual Delivery",
         default=False,
-        help="If Manual, the deliveries are not created at SO confirmation.\
-        You need to use the Create Delivery button in order to reserve and \
-        ship the goods.",
+        help="If Manual, the deliveries are not created at SO confirmation. "
+        "You need to use the Create Delivery button in order to reserve "
+        "and ship the goods.",
     )
 
     @api.onchange("team_id")
     def _onchange_team_id(self):
-        for so in self:
-            if so.team_id:
-                so.manual_delivery = so.team_id.manual_delivery
-            else:
-                so.manual_delivery = False
+        self.manual_delivery = self.team_id.manual_delivery
 
-    @api.multi
     def action_manual_delivery_wizard(self):
         self.ensure_one()
-        wizard = self.env["manual.delivery"].create({"carrier_id": self.carrier_id.id})
-        action = self.env.ref(
-            "sale_manual_delivery.action_wizard_manual_delivery"
-        ).read()[0]
-        action["res_id"] = wizard.id
+        action = self.env.ref("sale_manual_delivery.action_wizard_manual_delivery")
+        [action] = action.read()
+        action["context"] = {"default_carrier_id": self.carrier_id.id}
         return action
 
-    @api.multi
-    def toggle_manual(self):
-        self.ensure_one()
-        if isinstance(self.id, int):  # if already saved
-            if self.state not in ("draft", "sent"):
-                raise UserError(
-                    _(
-                        "You can only change to/from manual delivery"
-                        " in a quote, not a confirmed order"
-                    )
+    @api.constrains("manual_delivery")
+    def _check_manual_delivery(self):
+        if any(rec.state not in ["draft", "sent"] for rec in self):
+            raise UserError(
+                _(
+                    "You can only change to/from manual delivery"
+                    " in a quote, not a confirmed order"
                 )
-            self.manual_delivery = not self.manual_delivery
+            )

sale_manual_delivery/wizard/manual_delivery.py
@@ -1,0 +1,90 @@
+# Copyright 2017 Denis Leemann, Camptocamp SA
+# Copyright 2021 Iván Todorovich, Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+
+
+class ManualDelivery(models.TransientModel):
+    _name = "manual.delivery"
+    _description = "Manual Delivery"
+    _order = "create_date desc"
+
+    @api.model
+    def default_get(self, fields):
+        if "line_ids" not in fields:
+            return {}
+        res = super().default_get(fields)
+        # Get lines from active_model if it's a sale.order / sale.order.line
+        sale_lines = self.env["sale.order.line"]
+        active_model = self.env.context["active_model"]
+        if active_model == "sale.order.line":
+            sale_ids = self.env.context["active_ids"] or []
+            sale_lines = self.env["sale.order.line"].browse(sale_ids)
+        elif active_model == "sale.order":
+            sale_ids = self.env.context["active_ids"] or []
+            sale_lines = self.env["sale.order"].browse(sale_ids).mapped("order_line")
+        if len(sale_lines.mapped("order_id.partner_id")) > 1:
+            raise UserError(_("Please select one partner at a time"))
+        if sale_lines:
+            # Get partner from those lines
+            partner = sale_lines.mapped("order_id.partner_id")
+            res["partner_id"] = partner.id
+            res["commercial_partner_id"] = partner.commercial_partner_id.id
+            # Convert to manual.delivery.lines
+            res["line_ids"] = [
+                (
+                    0,
+                    0,
+                    {
+                        "order_line_id": line.id,
+                        "name": line.name,
+                        "product_id": line.product_id.id,
+                        "qty_ordered": line.product_uom_qty,
+                        "qty_planned": line.qty_planned,
+                        "quantity": line.qty_to_plan,
+                    },
+                )
+                for line in sale_lines
+                if line.qty_to_plan and line.product_id.type != "service"
+            ]
+        return res
+
+    commercial_partner_id = fields.Many2one(
+        "res.partner", required=True, readonly=True, ondelete="cascade",
+    )
+    partner_id = fields.Many2one(
+        "res.partner",
+        string="Delivery Address",
+        domain="""
+            [
+                "|",
+                ("id", "=", commercial_partner_id),
+                ("parent_id", "=", commercial_partner_id),
+            ],
+        """,
+        ondelete="cascade",
+    )
+    carrier_id = fields.Many2one(
+        "delivery.carrier", string="Delivery Method", ondelete="cascade",
+    )
+    route_id = fields.Many2one(
+        "stock.location.route",
+        string="Use specific Route",
+        domain=[("sale_selectable", "=", True)],
+        ondelete="cascade",
+        help="Leave it blank to use the same route that is in the sale line",
+    )
+    line_ids = fields.One2many(
+        "manual.delivery.line", "manual_delivery_id", string="Lines to validate",
+    )
+    date_planned = fields.Datetime(string="Date Planned")
+
+    def confirm(self):
+        """ Creates the manual procurements """
+        self.ensure_one()
+        sale_order_lines = self.line_ids.mapped("order_line_id")
+        sale_order_lines.with_context(
+            sale_manual_delivery=self
+        )._action_launch_stock_rule_manual()

sale_manual_delivery/wizard/manual_delivery_line.py
@@ -1,0 +1,56 @@
+# Copyright 2017 Denis Leemann, Camptocamp SA
+# Copyright 2021 Iván Todorovich, Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+from odoo.tools import float_compare
+
+
+class ManualDeliveryLine(models.TransientModel):
+    _name = "manual.delivery.line"
+    _description = "Manual Delivery Line"
+
+    manual_delivery_id = fields.Many2one(
+        "manual.delivery",
+        string="Wizard",
+        ondelete="cascade",
+        required=True,
+        readonly=True,
+    )
+    order_line_id = fields.Many2one(
+        "sale.order.line",
+        string="Sale Order Line",
+        required=True,
+        readonly=True,
+        ondelete="cascade",
+    )
+    product_id = fields.Many2one(related="order_line_id.product_id")
+    name = fields.Text(related="order_line_id.name")
+    qty_ordered = fields.Float(
+        string="Ordered",
+        related="order_line_id.product_uom_qty",
+        help="Quantity ordered in the related Sale Order",
+        readonly=True,
+    )
+    qty_planned = fields.Float(related="order_line_id.qty_planned")
+    quantity = fields.Float()
+
+    @api.constrains("quantity")
+    def _check_quantity(self):
+        """ Prevent delivering more than the ordered quantity """
+        if any(
+            float_compare(
+                line.quantity,
+                line.qty_ordered - line.qty_planned,
+                precision_rounding=line.product_id.uom_id.rounding,
+            )
+            > 0.00
+            for line in self
+        ):
+            raise UserError(
+                _(
+                    "You can not deliver more than the remaining quantity. "
+                    "If you need to do so, please edit the sale order first."
+                )
+            )

adhoc-cicd/oca-sale-workflow

sale_manual_delivery/models/crm_team.py
@@ -1,5 +1,6 @@
 # Copyright 2017 Denis Leemann, Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from odoo import fields, models
 
 
@@ -8,8 +9,8 @@ class CrmTeam(models.Model):
 
     manual_delivery = fields.Boolean(
         string="Manual Delivery",
+        help="If enabled, the deliveries are not created at SO confirmation. "
+        "You need to use the Create Delivery button in order to reserve and "
+        "ship the goods.",
         default=False,
-        help="If Manual, the deliveries are not created at SO confirmation.\
-        You need to use the Create Delivery button in order to reserve and \
-        ship the goods.",
     )

sale_manual_delivery/models/sale_order.py
@@ -1,8 +1,9 @@
 # Copyright 2017 Denis Leemann, Camptocamp SA
+# Copyright 2021 Iván Todorovich, Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import api, fields, models
+
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
-from odoo.tools.translate import _
 
 
 class SaleOrder(models.Model):
@@ -11,38 +12,28 @@ class SaleOrder(models.Model):
     manual_delivery = fields.Boolean(
         string="Manual Delivery",
         default=False,
-        help="If Manual, the deliveries are not created at SO confirmation.\
-        You need to use the Create Delivery button in order to reserve and \
-        ship the goods.",
+        help="If enabled, the deliveries are not created at SO confirmation. "
+        "You need to use the Create Delivery button in order to reserve "
+        "and ship the goods.",
     )
 
     @api.onchange("team_id")
     def _onchange_team_id(self):
-        for so in self:
-            if so.team_id:
-                so.manual_delivery = so.team_id.manual_delivery
-            else:
-                so.manual_delivery = False
+        self.manual_delivery = self.team_id.manual_delivery
 
-    @api.multi
     def action_manual_delivery_wizard(self):
         self.ensure_one()
-        wizard = self.env["manual.delivery"].create({"carrier_id": self.carrier_id.id})
-        action = self.env.ref(
-            "sale_manual_delivery.action_wizard_manual_delivery"
-        ).read()[0]
-        action["res_id"] = wizard.id
+        action = self.env.ref("sale_manual_delivery.action_wizard_manual_delivery")
+        [action] = action.read()
+        action["context"] = {"default_carrier_id": self.carrier_id.id}
         return action
 
-    @api.multi
-    def toggle_manual(self):
-        self.ensure_one()
-        if isinstance(self.id, int):  # if already saved
-            if self.state not in ("draft", "sent"):
-                raise UserError(
-                    _(
-                        "You can only change to/from manual delivery"
-                        " in a quote, not a confirmed order"
-                    )
+    @api.constrains("manual_delivery")
+    def _check_manual_delivery(self):
+        if any(rec.state not in ["draft", "sent"] for rec in self):
+            raise UserError(
+                _(
+                    "You can only change to/from manual delivery"
+                    " in a quote, not a confirmed order"
                 )
-            self.manual_delivery = not self.manual_delivery
+            )

sale_manual_delivery/wizard/manual_delivery.py
@@ -1,0 +1,88 @@
+# Copyright 2017 Denis Leemann, Camptocamp SA
+# Copyright 2021 Iván Todorovich, Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+
+
+class ManualDelivery(models.TransientModel):
+    _name = "manual.delivery"
+    _description = "Manual Delivery"
+    _order = "create_date desc"
+
+    @api.model
+    def default_get(self, fields):
+        res = super().default_get(fields)
+        # Get lines from active_model if it's a sale.order / sale.order.line
+        sale_lines = self.env["sale.order.line"]
+        active_model = self.env.context["active_model"]
+        if active_model == "sale.order.line":
+            sale_ids = self.env.context["active_ids"] or []
+            sale_lines = self.env["sale.order.line"].browse(sale_ids)
+        elif active_model == "sale.order":
+            sale_ids = self.env.context["active_ids"] or []
+            sale_lines = self.env["sale.order"].browse(sale_ids).mapped("order_line")
+        if len(sale_lines.mapped("order_id.partner_id")) > 1:
+            raise UserError(_("Please select one partner at a time"))
+        if sale_lines:
+            # Get partner from those lines
+            partner = sale_lines.mapped("order_id.partner_id")
+            res["partner_id"] = partner.id
+            res["commercial_partner_id"] = partner.commercial_partner_id.id
+            # Convert to manual.delivery.lines
+            res["line_ids"] = [
+                (
+                    0,
+                    0,
+                    {
+                        "order_line_id": line.id,
+                        "name": line.name,
+                        "product_id": line.product_id.id,
+                        "qty_ordered": line.product_uom_qty,
+                        "qty_procured": line.qty_procured,
+                        "quantity": line.qty_to_procure,
+                    },
+                )
+                for line in sale_lines
+                if line.qty_to_procure and line.product_id.type != "service"
+            ]
+        return res
+
+    commercial_partner_id = fields.Many2one(
+        "res.partner", required=True, readonly=True, ondelete="cascade",
+    )
+    partner_id = fields.Many2one(
+        "res.partner",
+        string="Delivery Address",
+        domain="""
+            [
+                "|",
+                ("id", "=", commercial_partner_id),
+                ("parent_id", "=", commercial_partner_id),
+            ],
+        """,
+        ondelete="cascade",
+    )
+    carrier_id = fields.Many2one(
+        "delivery.carrier", string="Delivery Method", ondelete="cascade",
+    )
+    route_id = fields.Many2one(
+        "stock.location.route",
+        string="Use specific Route",
+        domain=[("sale_selectable", "=", True)],
+        ondelete="cascade",
+        help="Leave it blank to use the same route that is in the sale line",
+    )
+    line_ids = fields.One2many(
+        "manual.delivery.line", "manual_delivery_id", string="Lines to validate",
+    )
+    date_planned = fields.Datetime(string="Date Planned")
+
+    def confirm(self):
+        """ Creates the manual procurements """
+        self.ensure_one()
+        sale_order_lines = self.line_ids.mapped("order_line_id")
+        sale_order_lines.with_context(
+            sale_manual_delivery=self
+        )._action_launch_stock_rule_manual()

sale_manual_delivery/wizard/manual_delivery_line.py
@@ -1,0 +1,56 @@
+# Copyright 2017 Denis Leemann, Camptocamp SA
+# Copyright 2021 Iván Todorovich, Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+from odoo.tools import float_compare
+
+
+class ManualDeliveryLine(models.TransientModel):
+    _name = "manual.delivery.line"
+    _description = "Manual Delivery Line"
+
+    manual_delivery_id = fields.Many2one(
+        "manual.delivery",
+        string="Wizard",
+        ondelete="cascade",
+        required=True,
+        readonly=True,
+    )
+    order_line_id = fields.Many2one(
+        "sale.order.line",
+        string="Sale Order Line",
+        required=True,
+        readonly=True,
+        ondelete="cascade",
+    )
+    product_id = fields.Many2one(related="order_line_id.product_id")
+    name = fields.Text(related="order_line_id.name")
+    qty_ordered = fields.Float(
+        string="Ordered",
+        related="order_line_id.product_uom_qty",
+        help="Quantity ordered in the related Sale Order",
+        readonly=True,
+    )
+    qty_procured = fields.Float(related="order_line_id.qty_procured")
+    quantity = fields.Float()
+
+    @api.constrains("quantity")
+    def _check_quantity(self):
+        """ Prevent delivering more than the ordered quantity """
+        if any(
+            float_compare(
+                line.quantity,
+                line.qty_ordered - line.qty_procured,
+                precision_rounding=line.product_id.uom_id.rounding,
+            )
+            > 0.00
+            for line in self
+        ):
+            raise UserError(
+                _(
+                    "You can not deliver more than the remaining quantity. "
+                    "If you need to do so, please edit the sale order first."
+                )
+            )