OCA/sale-workflow

[REF] sale_automatic_workflow|_stock: separate
There is an issue for service companies that do not need the stock
module to be installed when using `sale_automatic_workflow`. Module
depended on `stock`, therefore it was auto installed. Now, stock related
part is moved to a separate module - `sale_automatic_workflow_stock` and
can be installed separately.

sale_automatic_workflow/tests/common.py
@@ -13,13 +13,15 @@ class TestCommon(SavepointCase):
 
 
 class TestAutomaticWorkflowMixin(object):
-    def create_sale_order(self, workflow, override=None):
+
+    def create_sale_order(self, workflow, override=None, product_type='consu'):
         sale_obj = self.env["sale.order"]
 
         partner_values = {"name": "Imperator Caius Julius Caesar Divus"}
         partner = self.env["res.partner"].create(partner_values)
 
-        product_values = {"name": "Bread", "list_price": 5, "type": "product"}
+        product_values = {
+            "name": "Bread", "list_price": 5, "type": product_type}
         product = self.env["product.product"].create(product_values)
         self.product_uom_unit = self.env.ref("uom.product_uom_unit")
         values = {
@@ -41,42 +43,14 @@ class TestAutomaticWorkflowMixin(object):
         }
         if override:
             values.update(override)
-        order = sale_obj.create(values)
-        # Create inventory for add stock qty to lines
-        # With this commit https://goo.gl/fRTLM3 the moves that where
-        # force-assigned are not transferred in the picking
-        for line in order.order_line:
-            if line.product_id.type == "product":
-                inventory = self.env["stock.inventory"].create(
-                    {
-                        "name": "Inventory for move %s" % line.name,
-                        "line_ids": [
-                            (
-                                0,
-                                0,
-                                {
-                                    "product_id": line.product_id.id,
-                                    "product_qty": line.product_uom_qty,
-                                    "location_id": self.env.ref(
-                                        "stock.stock_location_stock"
-                                    ).id,
-                                },
-                            )
-                        ],
-                    }
-                )
-                inventory.action_start()
-                inventory.action_validate()
-        return order
+        return sale_obj.create(values)
 
     def create_full_automatic(self, override=None):
         workflow_obj = self.env["sale.workflow.process"]
         values = workflow_obj.create(
             {
                 "name": "Full Automatic",
-                "picking_policy": "one",
                 "validate_order": True,
-                "validate_picking": True,
                 "create_invoice": True,
                 "validate_invoice": True,
                 "invoice_date_is_order_date": True,
@@ -88,3 +62,127 @@ class TestAutomaticWorkflowMixin(object):
 
     def run_job(self):
         self.env["automatic.workflow.job"].run()
+
+
+class TestMultiCompanyCommon(TestCommon):
+    """Common class for multi-company related tests."""
+
+    @classmethod
+    def create_company(cls, values):
+        return cls.env["res.company"].create(values)
+
+    @classmethod
+    def create_product(cls, values):
+        values.update({"type": "consu", "invoice_policy": "order"})
+        product_template = cls.env["product.template"].create(values)
+        return product_template.product_variant_id
+
+    @classmethod
+    def setUpClass(cls):
+        """Setup data for all test cases."""
+        super().setUpClass()
+        coa = cls.env.user.company_id.chart_template_id
+        cls.company_fr = cls.create_company(
+            {
+                "name": "French company",
+                "currency_id": cls.env.ref("base.EUR").id,
+                "country_id": cls.env.ref("base.fr").id,
+            }
+        )
+
+        cls.company_ch = cls.create_company(
+            {
+                "name": "Swiss company",
+                "currency_id": cls.env.ref("base.CHF").id,
+                "country_id": cls.env.ref("base.ch").id,
+            }
+        )
+
+        cls.company_be = cls.create_company(
+            {
+                "name": "Belgian company",
+                "currency_id": cls.env.ref("base.EUR").id,
+                "country_id": cls.env.ref("base.be").id,
+            }
+        )
+
+        cls.company_fr_daughter = cls.create_company(
+            {
+                "name": "French company daughter",
+                "currency_id": cls.env.ref("base.EUR").id,
+                "country_id": cls.env.ref("base.fr").id,
+            }
+        )
+
+        cls.env.user.company_ids |= cls.company_fr
+        cls.env.user.company_ids |= cls.company_ch
+        cls.env.user.company_ids |= cls.company_be
+        cls.env.user.company_ids |= cls.company_fr_daughter
+
+        cls.env.user.company_id = cls.company_fr.id
+        coa.try_loading_for_current_company()
+        cls.customer_fr = cls.env["res.partner"].create({"name": "Customer FR"})
+        cls.product_fr = cls.create_product({"name": "Evian bottle", "list_price": 2.0})
+
+        cls.env.user.company_id = cls.company_ch.id
+        coa.try_loading_for_current_company()
+        cls.customer_ch = cls.env["res.partner"].create({"name": "Customer CH"})
+        cls.product_ch = cls.create_product(
+            {"name": "Henniez bottle", "list_price": 3.0}
+        )
+
+        cls.env.user.company_id = cls.company_be.id
+        coa.try_loading_for_current_company()
+        cls.customer_be = cls.env["res.partner"].create({"name": "Customer BE"})
+        cls.product_be = (
+            cls.env["product.template"]
+            .create(
+                {
+                    "name": "SPA bottle",
+                    "list_price": 1.5,
+                    "type": "consu",
+                    "invoice_policy": "order",
+                }
+            )
+            .product_variant_id
+        )
+
+        cls.env.user.company_id = cls.company_fr_daughter.id
+        coa.try_loading_for_current_company()
+        cls.customer_fr_daughter = cls.env["res.partner"].create(
+            {"name": "Customer FR Daughter"}
+        )
+        cls.product_fr_daughter = cls.create_product(
+            {"name": "Contrex bottle", "list_price": 1.5}
+        )
+
+        cls.auto_wkf = cls.env.ref("sale_automatic_workflow.automatic_validation")
+        cls.env.user.company_id = cls.env.ref("base.main_company")
+
+    def create_auto_wkf_order(self, company, customer, product, qty):
+        SaleOrder = self.env["sale.order"]
+
+        self.product_uom_unit = self.env.ref("uom.product_uom_unit")
+
+        order = SaleOrder.create(
+            {
+                "partner_id": customer.id,
+                "company_id": company.id,
+                "workflow_process_id": self.auto_wkf.id,
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": product.name,
+                            "product_id": product.id,
+                            "price_unit": product.list_price,
+                            "product_uom_qty": qty,
+                            "product_uom": self.product_uom_unit.id,
+                        },
+                    )
+                ],
+            }
+        )
+        order._onchange_workflow_process_id()
+        return order

adhoc-cicd/oca-sale-workflow

sale_automatic_workflow/tests/common.py
@@ -2,7 +2,10 @@
 # Copyright 2020 Camptocamp SA (author: Simone Orsi)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from odoo.tests import tagged
 from odoo.tests.common import TransactionCase
+
+from odoo.addons.account.tests.common import AccountTestInvoicingCommon
 
 
 class TestCommon(TransactionCase):
@@ -13,13 +16,13 @@ class TestCommon(TransactionCase):
 
 
 class TestAutomaticWorkflowMixin:
-    def create_sale_order(self, workflow, override=None):
+    def create_sale_order(self, workflow, override=None, product_type="consu"):
         sale_obj = self.env["sale.order"]
 
         partner_values = {"name": "Imperator Caius Julius Caesar Divus"}
         partner = self.env["res.partner"].create(partner_values)
 
-        product_values = {"name": "Bread", "list_price": 5, "type": "product"}
+        product_values = {"name": "Bread", "list_price": 5, "type": product_type}
         product = self.env["product.product"].create(product_values)
         self.product_uom_unit = self.env.ref("uom.product_uom_unit")
         values = {
@@ -41,28 +44,14 @@ class TestAutomaticWorkflowMixin:
         }
         if override:
             values.update(override)
-        order = sale_obj.create(values)
-        # Create inventory
-        for line in order.order_line:
-            if line.product_id.type == "product":
-                inventory = self.env["stock.quant"].create(
-                    {
-                        "product_id": line.product_id.id,
-                        "location_id": self.env.ref("stock.stock_location_stock").id,
-                        "inventory_quantity": line.product_uom_qty,
-                    }
-                )
-                inventory._apply_inventory()
-        return order
+        return sale_obj.create(values)
 
     def create_full_automatic(self, override=None):
         workflow_obj = self.env["sale.workflow.process"]
         values = workflow_obj.create(
             {
                 "name": "Full Automatic",
-                "picking_policy": "one",
                 "validate_order": True,
-                "validate_picking": True,
                 "create_invoice": True,
                 "validate_invoice": True,
                 "invoice_date_is_order_date": True,
@@ -74,3 +63,132 @@ class TestAutomaticWorkflowMixin:
 
     def run_job(self):
         self.env["automatic.workflow.job"].run()
+
+
+@tagged("post_install", "-at_install")
+class TestMultiCompanyCommon(AccountTestInvoicingCommon):
+    @classmethod
+    def create_product(cls, values):
+        values.update({"type": "consu", "invoice_policy": "order"})
+        product_template = cls.env["product.template"].create(values)
+        return product_template.product_variant_id
+
+    @classmethod
+    def setUpClass(cls, chart_template_ref=None):
+        super().setUpClass()
+        cls.env = cls.env(
+            context=dict(
+                cls.env.context,
+                tracking_disable=True,
+                # Compatibility with sale_automatic_workflow_job: even if
+                # the module is installed, ensure we don't delay a job.
+                # Thus, we test the usual flow.
+                _job_force_sync=True,
+            )
+        )
+        cls.company_fr = cls.setup_company_data(
+            {
+                "name": "French company",
+                "currency_id": cls.env.ref("base.EUR").id,
+                "country_id": cls.env.ref("base.fr").id,
+            }
+        )["company"]
+
+        cls.company_ch = cls.setup_company_data(
+            {
+                "name": "Swiss company",
+                "currency_id": cls.env.ref("base.CHF").id,
+                "country_id": cls.env.ref("base.ch").id,
+            }
+        )["company"]
+
+        cls.company_be = cls.setup_company_data(
+            {
+                "name": "Belgian company",
+                "currency_id": cls.env.ref("base.EUR").id,
+                "country_id": cls.env.ref("base.be").id,
+            }
+        )["company"]
+
+        cls.company_fr_daughter = cls.setup_company_data(
+            {
+                "name": "French company daughter",
+                "currency_id": cls.env.ref("base.EUR").id,
+                "country_id": cls.env.ref("base.fr").id,
+            }
+        )["company"]
+
+        cls.env.user.company_ids |= cls.company_fr
+        cls.env.user.company_ids |= cls.company_ch
+        cls.env.user.company_ids |= cls.company_be
+        cls.env.user.company_ids |= cls.company_fr_daughter
+
+        cls.env.user.company_id = cls.company_fr.id
+        cls.customer_fr = (
+            cls.env["res.partner"]
+            .with_context(default_company_id=cls.company_fr.id)
+            .create({"name": "Customer FR"})
+        )
+        cls.product_fr = cls.create_product({"name": "Evian bottle", "list_price": 2.0})
+
+        cls.env.user.company_id = cls.company_ch.id
+
+        cls.customer_ch = cls.env["res.partner"].create({"name": "Customer CH"})
+        cls.product_ch = cls.create_product(
+            {"name": "Henniez bottle", "list_price": 3.0}
+        )
+
+        cls.env.user.company_id = cls.company_be.id
+        cls.customer_be = cls.env["res.partner"].create({"name": "Customer BE"})
+        cls.product_be = (
+            cls.env["product.template"]
+            .create(
+                {
+                    "name": "SPA bottle",
+                    "list_price": 1.5,
+                    "type": "consu",
+                    "invoice_policy": "order",
+                }
+            )
+            .product_variant_id
+        )
+
+        cls.env.user.company_id = cls.company_fr_daughter.id
+        cls.customer_fr_daughter = cls.env["res.partner"].create(
+            {"name": "Customer FR Daughter"}
+        )
+        cls.product_fr_daughter = cls.create_product(
+            {"name": "Contrex bottle", "list_price": 1.5}
+        )
+
+        cls.auto_wkf = cls.env.ref("sale_automatic_workflow.automatic_validation")
+        cls.env.user.company_id = cls.env.ref("base.main_company")
+
+    def create_auto_wkf_order(self, company, customer, product, qty):
+        # We need to change to the proper company
+        # to pick up correct company dependent fields
+        SaleOrder = self.env["sale.order"].with_company(company)
+
+        self.product_uom_unit = self.env.ref("uom.product_uom_unit")
+
+        order = SaleOrder.create(
+            {
+                "partner_id": customer.id,
+                "company_id": company.id,
+                "workflow_process_id": self.auto_wkf.id,
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": product.name,
+                            "product_id": product.id,
+                            "price_unit": product.list_price,
+                            "product_uom_qty": qty,
+                            "product_uom": self.product_uom_unit.id,
+                        },
+                    )
+                ],
+            }
+        )
+        return order