OCA/sale-workflow

[FIX] sale_order_type: Inconsistent data in multi-company environments

sale_order_type/models/sale_order_type.py
@@ -6,6 +6,7 @@ from odoo import api, fields, models
 class SaleOrderTypology(models.Model):
     _name = "sale.order.type"
     _description = "Type of sale order"
+    _check_company_auto = True
 
     @api.model
     def _get_domain_sequence_id(self):
@@ -18,6 +19,7 @@ class SaleOrderTypology(models.Model):
             "picking_policy"
         ]["selection"]
 
+    @api.model
     def default_picking_policy(self):
         default_dict = self.env["sale.order"].default_get(["picking_policy"])
         return default_dict.get("picking_policy")
@@ -33,24 +35,27 @@ class SaleOrderTypology(models.Model):
     journal_id = fields.Many2one(
         comodel_name="account.journal",
         string="Billing Journal",
-        domain=[("type", "=", "sale")],
+        domain="[('type', '=', 'sale'), '|', ('company_id', '=', False), "
+        "('company_id', '=', company_id)]",
+        check_company=True,
     )
-    warehouse_id = fields.Many2one(comodel_name="stock.warehouse", string="Warehouse")
+    warehouse_id = fields.Many2one(
+        comodel_name="stock.warehouse", string="Warehouse", check_company=True
+    )
     picking_policy = fields.Selection(
         selection="_get_selection_picking_policy",
         string="Shipping Policy",
         default=default_picking_policy,
     )
     company_id = fields.Many2one(
-        comodel_name="res.company",
-        related="warehouse_id.company_id",
-        store=True,
-        readonly=True,
+        comodel_name="res.company", default=lambda self: self.env.company, store=True,
     )
     payment_term_id = fields.Many2one(
-        comodel_name="account.payment.term", string="Payment Term"
+        comodel_name="account.payment.term", string="Payment Term", check_company=True
     )
-    pricelist_id = fields.Many2one(comodel_name="product.pricelist", strint="Pricelist")
+    pricelist_id = fields.Many2one(
+        comodel_name="product.pricelist", strint="Pricelist", check_company=True
+    )
     incoterm_id = fields.Many2one(comodel_name="account.incoterms", string="Incoterm")
     route_id = fields.Many2one(
         "stock.location.route",

adhoc-cicd/oca-sale-workflow

sale_order_type/models/sale_order_type.py
@@ -6,6 +6,7 @@ from odoo import api, fields, models
 class SaleOrderTypology(models.Model):
     _name = "sale.order.type"
     _description = "Type of sale order"
+    _check_company_auto = True
 
     @api.model
     def _get_domain_sequence_id(self):
@@ -18,6 +19,7 @@ class SaleOrderTypology(models.Model):
             "picking_policy"
         ]["selection"]
 
+    @api.model
     def default_picking_policy(self):
         default_dict = self.env["sale.order"].default_get(["picking_policy"])
         return default_dict.get("picking_policy")
@@ -33,9 +35,13 @@ class SaleOrderTypology(models.Model):
     journal_id = fields.Many2one(
         comodel_name="account.journal",
         string="Billing Journal",
-        domain=[("type", "=", "sale")],
+        domain="[('type', '=', 'sale'), '|', ('company_id', '=', False), "
+        "('company_id', '=', company_id)]",
+        check_company=True,
     )
-    warehouse_id = fields.Many2one(comodel_name="stock.warehouse", string="Warehouse")
+    warehouse_id = fields.Many2one(
+        comodel_name="stock.warehouse", string="Warehouse", check_company=True
+    )
     picking_policy = fields.Selection(
         selection="_get_selection_picking_policy",
         string="Shipping Policy",
@@ -43,14 +49,15 @@ class SaleOrderTypology(models.Model):
     )
     company_id = fields.Many2one(
         comodel_name="res.company",
-        related="warehouse_id.company_id",
+        default=lambda self: self.env.company,
         store=True,
-        readonly=True,
     )
     payment_term_id = fields.Many2one(
-        comodel_name="account.payment.term", string="Payment Term"
+        comodel_name="account.payment.term", string="Payment Term", check_company=True
     )
-    pricelist_id = fields.Many2one(comodel_name="product.pricelist", string="Pricelist")
+    pricelist_id = fields.Many2one(
+        comodel_name="product.pricelist", string="Pricelist", check_company=True
+    )
     incoterm_id = fields.Many2one(comodel_name="account.incoterms", string="Incoterm")
     route_id = fields.Many2one(
         "stock.location.route",