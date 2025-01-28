akretion/ak-odoo-incubator

[IMP] refactor to use a computed field with a close behaviour as invoice_status

sale_picking_state/tests/test_delivery_state.py
@@ -1,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Akretion (http://www.akretion.com).
+# @author Benoît GUILLOT <benoit.guillot@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.tests.common import TransactionCase
+
+
+class TestDeliveryState(TransactionCase):
+
+    def setUp(self):
+        super(TestDeliveryState, self).setUp()
+        self.order = self.env.ref('sale_picking_state.sale_order_1')
+
+    def test_no_delivery(self):
+        self.assertEqual(self.order.delivery_state, 'no')
+
+    def test_unprocessed_delivery(self):
+        self.order.action_confirm()
+        self.assertEqual(self.order.delivery_state, 'unprocessed')
+
+    def test_partially(self):
+        self.order.action_confirm()
+        self.order.order_line[0].qty_delivered = 2
+        self.assertEqual(self.order.delivery_state, 'partially')
+
+    def test_delivery_done(self):
+        self.order.action_confirm()
+        for line in self.order.order_line:
+            line.qty_delivered = line.product_uom_qty
+        self.assertEqual(self.order.delivery_state, 'done')

adhoc-cicd/oca-sale-workflow

sale_delivery_state/tests/test_delivery_state.py
@@ -1,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Akretion (http://www.akretion.com).
+# @author Benoît GUILLOT <benoit.guillot@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.tests.common import TransactionCase
+
+
+class TestDeliveryState(TransactionCase):
+
+    def setUp(self):
+        super(TestDeliveryState, self).setUp()
+        self.order = self.env.ref('sale_delivery_state.sale_order_1')
+
+    def test_no_delivery(self):
+        self.assertEqual(self.order.delivery_state, 'no')
+
+    def test_unprocessed_delivery(self):
+        self.order.action_confirm()
+        self.assertEqual(self.order.delivery_state, 'unprocessed')
+
+    def test_partially(self):
+        self.order.action_confirm()
+        self.order.order_line[0].qty_delivered = 2
+        self.assertEqual(self.order.delivery_state, 'partially')
+
+    def test_delivery_done(self):
+        self.order.action_confirm()
+        for line in self.order.order_line:
+            line.qty_delivered = line.product_uom_qty
+        self.assertEqual(self.order.delivery_state, 'done')